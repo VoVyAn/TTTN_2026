3d6ade50373913c199c6e5e733279bdd2e36c529
VoVyAn <vovyan2809@gmail.com>
2026-06-13 00:33:54 +0700

    update code

--- backend/fix_admin.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Admin = require('./models/Admin');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const result = await Admin.updateOne({ username: 'test1' }, { $set: { role: 'user' } });
-  console.log('Downgraded test1 to user:', result);
-  mongoose.disconnect();
-}).catch(console.error);
