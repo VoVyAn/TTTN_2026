b159fe081e945095fa5a64362110ef19e4516a8d
VoVyAn <vovyan2809@gmail.com>
2026-06-15 22:48:22 +0700

    update code

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -17,10 +17,16 @@ const createTransporter = async () => {
   // If Gmail configurations are present in .env, use them
   if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
     return nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+      },
+      tls: {
+        // Do not fail on invalid certs
+        rejectUnauthorized: false
       }
     });
   }
